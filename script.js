6ea15991a41ed51b04f8341ebee5445981e13334
Daniel <itzdanielmichael@gmail.com>
2021-08-03 12:52:13 -0400

    update index.html, script.js, style.css

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
-const boldBtn = document.querySelector("#bold-btn")
-const underlineBtn = document.querySelector("#underline-btn")
-const italicBtn = document.querySelector("#italic-btn")
-const colorBtn = document.querySelector("#color-btn")
+const newBtn = document.querySelector('#new-btn');
+const saveTxtBtn = document.querySelector('#txt-btn');
+const savePdfBtn = document.querySelector('#pdf-btn');
 
-const newBtn = document.querySelector("#new-btn")
-const txtBtn = document.querySelector("#txt-btn")
-const pdfBtn = document.querySelector("#pdf-btn")
+const content = document.querySelector('#content');
+const filename = document.querySelector('#filename-input');
 
-const content = document.querySelector("#content")
-const filename = document.querySelector("#filename-input")
+const controlButtons = document.getElementsByClassName('control-btn');
 
-boldBtn.addEventListener("click", () => {
-    document.execCommand("bold")
-})
+for (const btn of controlButtons) {
+  btn.addEventListener('click', () => {
+    let cmd = btn.dataset['command'];
+    switch (cmd) {
+      case 'forecolor':
+        document.execCommand('forecolor', false, btn.value);
+        break;
+      case 'createlink':
+        let url = prompt('Enter the link here: ', 'http://');
+        document.execCommand(cmd, false, url);
+        break;
+      default:
+        document.execCommand(cmd, false, null);
+    }
+  });
+}
 
-underlineBtn.addEventListener("click", () => {
-    document.execCommand("underline")
-})
+newBtn.addEventListener('click', () => {
+  content.innerHTML = '';
+});
 
-italicBtn.addEventListener("click", () => {
-    document.execCommand("italic")
-})
+saveTxtBtn.addEventListener('click', () => {
+  const a = document.createElement('a');
+  const blob = new Blob([content.innerText]);
+  const dataUrl = URL.createObjectURL(blob);
+  a.href = dataUrl;
+  a.download = filename.value + '.txt';
+  a.click();
+});
 
-colorBtn.addEventListener("input", () => {
-    document.execCommand("forecolor", false, colorBtn.value)
-})
-
-newBtn.addEventListener("click", () => {
-    content.innerHTML = ""
-})
-
-txtBtn.addEventListener("click", () => {
-    const a = document.createElement("a")
-    const blob = new Blob([content.innerText])
-    const dataUrl = URL.createObjectURL(blob)
-    a.href = dataUrl
-    a.download = filename.value + ".txt"
-    a.click()
-})
-
-pdfBtn.addEventListener('click', () => {
-    html2pdf().from(content).save(filename.value)
-})
+savePdfBtn.addEventListener('click', () => {
+  // saves to pdf, check htmltopdf.js, pretty neat!
+  html2pdf().from(content).save(filename.value);
+});
